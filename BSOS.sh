34c2023a09bd9b2a6abebe5b99dee269aa1a9adc
EmacsIsBetterThanVi <168662379+EmacsIsBetterThanVi@users.noreply.github.com>
2024-05-14 18:48:09 -0400

    Fixed a major bug BSOS.sh which prevented build from starting

--- BSOS.sh
+++ BSOS.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [[ $1 == "run" ]]; then
     shift
-    qemu-system-x86_64 -drive format=raw,file=BSOS.img,if=floppy,cache=writethrough $@
+    qemu-system-x86_64 -drive format=raw,file=BSOS.img,if=floppy boot= $@
 elif [[ $1 == "chdisk" ]]; then
     echo "BOOT SECTOR"
     dd if=BSOS.img bs=512 count=1 skip=0 | hexdump -C -v
     echo "FILES"
     dd if=BSOS.img bs=512 count=$2 skip=1 | hexdump -C -v
-elif [[ $tmp == "build" ]]; then
+elif [[ $1 == "build" ]]; then
     size=$2
     shift 2
     nasm -f bin BSOS.s -dVERSIONNUM=\"$(cat VERSION.txt)\" $@
@@ -17,6 +17,6 @@ elif [[ $tmp == "build" ]]; then
 	dd if=/dev/zero of=BSOS.img bs=512 count=$size oseek=1
 else
     echo "./BSOS.sh [run|chdisk|build]"
-    echo "chdisk [size]"
-    echo "build [size] [options]"
+    echo "chdisk [size] - Prints the disk contents"
+    echo "build [size] [options] - builds a BSOS img"
 fi
