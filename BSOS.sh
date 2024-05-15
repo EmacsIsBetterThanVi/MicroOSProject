f656ff342cba47e701fad2b34aa0bdfebef71f26
EmacsIsBetterThanVi <168662379+EmacsIsBetterThanVi@users.noreply.github.com>
2024-05-14 21:17:55 -0400

    Update BSOS.sh

--- BSOS.sh
+++ BSOS.sh
@@ -9,11 +9,12 @@ elif [[ $1 == "chdisk" ]]; then
     dd if=BSOS.img bs=512 count=$2 skip=1 | hexdump -C -v
 elif [[ $1 == "build" ]]; then
     size=$2
-    shift 2
+    shift
+    shift
     nasm -f bin BSOS.s -dVERSIONNUM=\"$(cat VERSION.txt)\" $@
-	hdiutil create -sectors 40 -fs "MS-DOS FAT12" -volname "BSOS" -nospotlight ~/BSOS.img
+	hdiutil create -sectors 40 -fs "MS-DOS FAT12" -volname "BSOS" -nospotlight BSOS.img
 	mv BSOS.img.dmg BSOS.img
-	dd if=~/BSOS of=BSOS.img bs=512 count=1 seek=0
+	dd if=BSOS of=BSOS.img bs=512 count=1 seek=0
 	dd if=/dev/zero of=BSOS.img bs=512 count=$size oseek=1
 else
     echo "./BSOS.sh [run|chdisk|build]"
